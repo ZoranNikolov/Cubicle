9ce3368d0cf35831546234a302e78787094ccfed
Zoran Nikolov <zoran902@gmail.com>
2023-02-09 16:22:00 +0200

    'updated'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Initial');
+const express = require("express");
+
+const config = require("./config.js");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Home page");
+});
+
+app.listen(config.PORT, () =>
+	console.log(`Server is running on port ${config.PORT}...`)
+);
