f4522cf0e2353e6b9500de898c88cb94483f16a9
Zoran Nikolov <zoran902@gmail.com>
2023-02-10 11:20:57 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
-const config = require("./config.js");
 const app = express();
+const config = require("./config");
+const setupViewEngine = require("./config/viewEngine");
+
+setupViewEngine(app);
 
 app.get("/", (req, res) => {
-	res.send("Home page");
+	res.render("home");
 });
 
 app.listen(config.PORT, () =>
