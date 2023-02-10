78a4704d613b85cc8d9aa57c2fce6d0760035dcb
Zoran Nikolov <zoran902@gmail.com>
2023-02-10 14:15:06 +0200

    add cube with persistance

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,3 +1,12 @@
+const Cube = require('../models/Cube')
+
 exports.getCreateCube = (req, res) => {
 	res.render("create");
+};
+
+exports.postCreateCube = (req, res) => {
+	let cube = new Cube(req.body);
+	Cube.save(cube);
+	
+	res.redirect('/');
 }
