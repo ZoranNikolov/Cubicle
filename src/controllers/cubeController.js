989d5a363b413b8ef69dec2c7b80cbc86ea67ad0
Zoran Nikolov <zoran902@gmail.com>
2023-02-10 16:13:18 +0200

    updated

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
-const Cube = require('../models/Cube')
+const Cube = require("../models/Cube");
+const db = require("../db.json");
 
 exports.getCreateCube = (req, res) => {
 	res.render("create");
@@ -10,5 +11,21 @@ exports.postCreateCube = (req, res) => {
 	let cube = new Cube(name, description, imageUrl, difficultyLevel);
 	Cube.save(cube);
 
-	res.redirect('/');
-}
+	res.redirect("/");
+};
+
+exports.getDetails = (req, res) => {
+	let cubeId = Number(req.params.cubeId);
+
+	if (!cubeId) {
+		return res.redirect("/404");
+	}
+
+	let cube = db.cubes.find((x) => x.id === cubeId);
+
+	if (!cube) {
+		return res.redirect("/404");
+	}
+
+	res.render("details", { cube });
+};
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,10 @@ const homeController = require("./controllers/homeController");
 
 router.get("/", homeController.getHomePage);
 router.get("/about", homeController.getAboutPage);
+router.get("/404", homeController.getErrorPage);
 
 router.get("/create", cubeController.getCreateCube);
-router.post('/create', cubeController.postCreateCube);
+router.post("/create", cubeController.postCreateCube);
+router.get("/details/:cubeId", cubeController.getDetails);
+
 module.exports = router;
