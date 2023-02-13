73791c10d0209c9d13741e03c02854df15876d00
Zoran Nikolov <zoran902@gmail.com>
2023-02-13 16:08:05 +0200

    show cubes from db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,8 @@
-const db = require("../db.json");
+const Cube = require('../models/Cube');
 
-exports.getHomePage = (req, res) => {
+exports.getHomePage = async (req, res) => {
 	const { search, from: difficultyFrom, to: difficultyTo } = req.query;
-	let cubes = db.cubes;
+	let cubes = await Cube.find().lean();
 
 	if (search) {
 		cubes = cubes.filter((cube) =>
