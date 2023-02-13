345d07453d540dca1ef6d74a87230508cf60270a
Zoran Nikolov <zoran902@gmail.com>
2023-02-13 16:19:22 +0200

    show details

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -15,14 +15,8 @@ exports.postCreateCube = async (req, res) => {
 	res.redirect("/");
 };
 
-exports.getDetails = (req, res) => {
-	let cubeId = Number(req.params.cubeId);
-
-	if (!cubeId) {
-		return res.redirect("/404");
-	}
-
-	let cube = db.cubes.find((x) => x.id === cubeId);
+exports.getDetails = async (req, res) => {
+	const cube = await Cube.findById(req.params.cubeId).lean();
 
 	if (!cube) {
 		return res.redirect("/404");
