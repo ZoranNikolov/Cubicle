e8a4bf173fb1977df9d9ca09e9d3f411aef51ae6
Zoran Nikolov <zoran902@gmail.com>
2023-02-13 15:48:52 +0200

    add mongoose

---
+++ src/config/databaseInit.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const config = require('./index')
+
+async function initDatabase(){
+	mongoose.set('strictQuery', false);
+	await mongoose.connect(config.DB_URI);
+
+	console.log('DB connected');
+}
+
+module.exports = initDatabase;
