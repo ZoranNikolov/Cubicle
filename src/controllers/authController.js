1617cd30c5966b27c6a0e14b509ffa986296a824
Zoran Nikolov <zoran902@gmail.com>
2023-02-15 17:03:16 +0200

    updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,12 +6,21 @@ router.get('/login', (req, res) => {
 	res.render('auth/login');
 });
 
+router.post('/login', async (req, res) => {
+	const { username, password } = req.body;
+
+	const user = await authService.login(username, password);
+	console.log(user);
+
+	res.redirect('/');
+});
+
 router.get('/register', (req, res) => {
 	res.render('auth/register');
 });
 
 router.post('/register', async (req, res) => {
-	const {username, password, repeatPassword} = req.body;
+	const { username, password, repeatPassword } = req.body;
 
 	if (password !== repeatPassword) {
 		return res.redirect("/404");
@@ -24,7 +33,6 @@ router.post('/register', async (req, res) => {
 	}
 
 	const user = await authService.register(username, password);
-
 	console.log(user);
 
 	res.redirect('/login');
