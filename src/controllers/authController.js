86ee9c4c148f166fa7c86a0ce57600087a6a5c81
Zoran Nikolov <zoran902@gmail.com>
2023-02-16 11:09:26 +0200

    generate token on login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,8 +9,8 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
 	const { username, password } = req.body;
 	try {
-		const user = await authService.login(username, password);
-		console.log(user);
+		const token = await authService.login(username, password);
+		console.log(token);
 	} catch (err) {
 		console.log(err);
 		return res.redirect('/');
